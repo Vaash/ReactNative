6feabcf063c354a9fcb9de598df087ddc2a522e5
Bastien Delboé <bastiendelboe@mbp-de-bastien.home>
2020-09-01 16:55:25 +0200

    going to Atom

---
+++ Components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Button, TextIntup } from 'react-native'
+
+class Search extends React.Component {
+    render() {
+        return (
+            <view>
+
+            </view>
+        )
+    }
+}
