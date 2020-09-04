a4516227f8fea6d144ddf83e70d4010d96951d99
Bastien Delboé <bastiendelboe@MBP-de-Bastien.home>
2020-09-04 16:51:05 +0200

    Done

--- Components/BreweryAdress.js
+++ Components/BreweryAdress.js
@@ -1,22 +1,32 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 
 class BreweryAdress extends React.Component {
     render() {
         const brewery = this.props.brewery
         return (
-            <View>
-                <Text>Adresse</Text>
+            <View style={styles.main_view_container}>
+                <Text style={styles.adresse}>Adresse</Text>
                 <Text>
-                    {brewery.street},
-                    {brewery.city},
-                    {brewery.postal_code},
-                    {brewery.state},
-                    {brewery.country}
+                    {brewery.street !== '' ? <Text>{brewery.street}</Text> : null }
+                    {brewery.city !== '' ? <Text><br/>{brewery.city} </Text> : null }
+                    {brewery.postal_code !== '' ? <Text>{brewery.postal_code} </Text> : null }
+                    {brewery.state !== '' ? <Text>{brewery.state}</Text> : null }
+                    {brewery.country !== '' ? <Text><br/>{brewery.country}</Text> : null }
                 </Text>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    main_view_container: {
+        padding: 10
+    },
+    adresse: {
+        fontWeight: 'bold',
+        paddingBottom: 10
+    }
+})
 
 export default BreweryAdress
--- Components/BreweryContact.js
+++ Components/BreweryContact.js
@@ -1,17 +1,36 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 
 class BreweryContact extends React.Component {
     render() {
         const brewery = this.props.brewery
         return (
-            <View>
-                <Text>Contact</Text>
-                <Text>Site internet<br/>{brewery.website_url}</Text>
-                <Text>Téléphone<br/>{brewery.phone}</Text>
+            <View style={styles.main_view_container}>
+                {brewery.phone && brewery.website_url !== '' ? <Text style={styles.contact}>Contact</Text> : null }
+                {brewery.website_url !== '' ? <Text style={styles.url}>Site internet:</Text> : null }
+                {brewery.website_url !== '' ? <Text>{brewery.website_url}</Text> : null }
+                {brewery.phone !== '' ? <Text style={styles.phone}>Téléphone:</Text> : null }
+                {brewery.phone !== '' ? <Text>{brewery.phone}</Text> : null }
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    main_view_container: {
+        padding: 10
+    },
+    contact: {
+        fontWeight: 'bold',
+        paddingBottom: 10
+    },
+    url: {
+        textDecorationLine: 'underline'
+    },
+    phone: {
+        paddingTop: 10,
+        textDecorationLine: 'underline'
+    }
+})
 
 export default BreweryContact
